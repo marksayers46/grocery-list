a06d4cb3707afaf6fc71e22131bb2c76675268ae
Mark Sayers <marksayers46@icloud.com>
2021-12-15 11:59:33 -0600

    setup localStorage to save state in handleCheck

--- src/Content.js
+++ src/Content.js
@@ -22,9 +22,10 @@ const Content = () => {
     ]);
 
     const handleCheck = (id) => {
-        // console.log({item: id})
+        console.log(`key: ${id}`)
         const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
         setItems(listItems);
+        localStorage.setItem('shoppingList', JSON.stringify(listItems));
     }
 
     return (
