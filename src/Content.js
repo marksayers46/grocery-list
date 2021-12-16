4bba9e4b6664878a346d5fc5b9d86cafb6bb54ed
Mark Sayers <marksayers46@icloud.com>
2021-12-16 13:04:33 -0600

    Added expression for your list is empty style

--- src/Content.js
+++ src/Content.js
@@ -37,26 +37,28 @@ const Content = () => {
 
     return (
         <main>
-            <ul>
-                {items.map((item) => (
-                    <li className="item" key={item.id}>
-                        <input
-                            type="checkbox"
-                            onChange={() => handleCheck(item.id)}
-                            checked={item.checked}
-                        />
-                        <label
-                            style={(item.checked) ? { textDecoration:'line-through' } : null}
-                            onDoubleClick={() => handleCheck(item.id)}
-                        >{item.item}</label>
-                        <FaTrashAlt
-                            onClick={() => handleDelete(item.id)} 
-                            role="button" 
-                            tabIndex="0" 
-                        />
-                    </li>    
-                ))}
-            </ul>
+            {items.length > 0 ? (
+                <ul>
+                    {items.map((item) => (
+                        <li className="item" key={item.id}>
+                            <input
+                                type="checkbox"
+                                onChange={() => handleCheck(item.id)}
+                                checked={item.checked}
+                            />
+                            <label
+                                style={(item.checked) ? { textDecoration:'line-through' } : null}
+                                onDoubleClick={() => handleCheck(item.id)}
+                            >{item.item}</label>
+                            <FaTrashAlt
+                                onClick={() => handleDelete(item.id)} 
+                                role="button" 
+                                tabIndex="0" 
+                            />
+                        </li>    
+                    ))}
+                </ul>
+            ) : (<p style={{ marginTop: '2rem'}}>Your list is empty.</p>)}
         </main>
     )
 }
