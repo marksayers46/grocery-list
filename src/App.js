54320dd72f96726746822a4161b8b6aa008196e5
Mark Sayers <marksayers46@icloud.com>
2021-12-20 12:50:17 -0600

    set up localStorage

--- src/App.js
+++ src/App.js
@@ -5,23 +5,7 @@ import AddItem from './AddItem';
 import { useState } from 'react';
 
 function App() {
-    const [items, setItems] = useState([
-      {
-          id: 1,
-          checked: false,
-          item: "Item 1"
-      },
-      {
-          id: 2,
-          checked: false,
-          item: "Item 2"
-      },
-      {
-          id: 3,
-          checked: false,
-          item: "Item 3"
-      }
-  ]);
+    const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppingList')));
   const [newItem, setNewItem] = useState('')
 
   const setAndSavedItems = (newItems) => {
