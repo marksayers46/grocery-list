a45040fe59ad89c9ce0830b0a3bba7f4346ef5fd
Mark Sayers <marksayers46@icloud.com>
2021-12-29 18:24:43 -0600

    working with fetching errors

--- src/App.js
+++ src/App.js
@@ -10,23 +10,31 @@ function App() {
   const API_URL = 'http://localhost:3500/items';
 
   const [items, setItems] = useState([]);
-  const [newItem, setNewItem] = useState('')
-  const [search, setSearch] = useState('')
+  const [newItem, setNewItem] = useState('');
+  const [search, setSearch] = useState('');
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     
     const fetchItems = async () => {
       try {
         const response = await fetch(API_URL);
+        if (!response.ok) throw Error('Did not receive expected data')
         const listItems = await response.json();
         console.log(listItems);
         setItems(listItems);
+        setFetchError(null);
       } catch (err) {
-          console.log(err.stack);
-      } 
+         setFetchError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
     }
-
-    (async () => await fetchItems())();
+    setTimeout(() => {
+      (async () => await fetchItems())();
+    }, 2000)
+    
   }, []);
 
   const addItem = (item) => {
@@ -69,11 +77,15 @@ function App() {
         search={search}
         setSearch={setSearch}
       />
-      <Content 
-        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
+      <main>
+        {isLoading && <p>Loading Items...</p>}
+        {fetchError && <p style={{color: 'red'}}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && <Content 
+          items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />}
+      </main>
       <Footer length={items.length} />
     </div>
   );
--- src/Content.js
+++ src/Content.js
@@ -5,7 +5,7 @@ const Content = ({ items, handleCheck, handleDelete }) => {
     
     return (
 
-        <main>
+        <>
             {items.length > 0 ? (
                 <ItemList 
                 items={items}
@@ -14,7 +14,7 @@ const Content = ({ items, handleCheck, handleDelete }) => {
                 />    
             ) : (<p style={{ marginTop: "2rem"}}>Your list is empty.</p>
             )}
-        </main>
+        </>
     )
 }
 
