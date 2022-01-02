aeb19112bdd41244ed8f18cd7613010fbfcf47b8
Mark Sayers <marksayers46@icloud.com>
2022-01-02 12:22:38 -0600

    added PATCH (update) to CRUD operation

--- src/App.js
+++ src/App.js
@@ -54,12 +54,22 @@ function App() {
     if (result) setFetchError(result);
   }
 
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     //console.log(`key: ${id}`)
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
     setItems(listItems);
 
-    const myItem = listItems.filter((item) => item.id === id)
+    const myItem = listItems.filter((item) => item.id === id);
+    const updateOptions = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ checked: myItem[0].checked })
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
   }
 
   const handleDelete = (id) => {
